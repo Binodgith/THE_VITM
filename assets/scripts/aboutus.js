3d3f51e521959a6f281121627ea4960706beab1f
Codegirl1626 <smrutim481@gmail.com>
2024-10-04 10:57:16 +0530

    js part added

--- assets/scripts/aboutus.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("more-info-btn").onclick = function() {
-    const moreInfoDiv = document.getElementById("more-info");
-    moreInfoDiv.classList.toggle("hidden");
-    this.textContent = moreInfoDiv.classList.contains("hidden") ? "Learn More" : "Show Less";
-};
