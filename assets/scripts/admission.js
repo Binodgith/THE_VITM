d4428219feba994178bf6ead6823b21a95c13ec3
unknown <dbinod2020@gmail.com>
2024-10-15 17:34:37 +0530

    almost to complete admission page

---
+++ assets/scripts/admission.js
@@ -0,0 +1,98 @@
+let admission_notic_list=[
+    {
+        notic_id:"an001",
+        notic_date:"20-08-2024",
+        notic_content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, voluptatem. Perferendis dolore placeat earum at!"
+    },
+    {
+        notic_id:"an002",
+        notic_date:"20-08-2024",
+        notic_content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, voluptatem. Perferendis dolore placeat earum at!"
+    },
+    {
+        notic_id:"an003",
+        notic_date:"20-08-2024",
+        notic_content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, voluptatem. Perferendis dolore placeat earum at!"
+    },
+    {
+        notic_id:"an004",
+        notic_date:"20-08-2024",
+        notic_content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, voluptatem. Perferendis dolore placeat earum at!"
+    },
+    {
+        notic_id:"an005",
+        notic_date:"20-08-2024",
+        notic_content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, voluptatem. Perferendis dolore placeat earum at!"
+    },
+    {
+        notic_id:"an006",
+        notic_date:"20-08-2024",
+        notic_content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, voluptatem. Perferendis dolore placeat earum at!"
+    },
+    {
+        notic_id:"an007",
+        notic_date:"20-08-2024",
+        notic_content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, voluptatem. Perferendis dolore placeat earum at!"
+    },
+    {
+        notic_id:"an008",
+        notic_date:"20-08-2024",
+        notic_content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, voluptatem. Perferendis dolore placeat earum at!"
+    },
+    {
+        notic_id:"an009",
+        notic_date:"20-08-2024",
+        notic_content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, voluptatem. Perferendis dolore placeat earum at!"
+    },
+    {
+        notic_id:"an0010",
+        notic_date:"20-08-2024",
+        notic_content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, voluptatem. Perferendis dolore placeat earum at!"
+    },
+    {
+        notic_id:"an0011",
+        notic_date:"20-08-2024",
+        notic_content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, voluptatem. Perferendis dolore placeat earum at!"
+    },
+    {
+        notic_id:"an0012",
+        notic_date:"20-08-2024",
+        notic_content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, voluptatem. Perferendis dolore placeat earum at!"
+    },
+    {
+        notic_id:"an0013",
+        notic_date:"20-08-2024",
+        notic_content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, voluptatem. Perferendis dolore placeat earum at!"
+    },
+    {
+        notic_id:"an0014",
+        notic_date:"20-08-2024",
+        notic_content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, voluptatem. Perferendis dolore placeat earum at!"
+    },
+    {
+        notic_id:"an0015",
+        notic_date:"20-08-2024",
+        notic_content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, voluptatem. Perferendis dolore placeat earum at!"
+    },
+    {
+        notic_id:"an0016",
+        notic_date:"20-08-2024",
+        notic_content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, voluptatem. Perferendis dolore placeat earum at!"
+    },
+    {
+        notic_id:"an0017",
+        notic_date:"20-08-2024",
+        notic_content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, voluptatem. Perferendis dolore placeat earum at!"
+    },
+    {
+        notic_id:"an0018",
+        notic_date:"20-08-2024",
+        notic_content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, voluptatem. Perferendis dolore placeat earum at!"
+    },
+    {
+        notic_id:"an0019",
+        notic_date:"20-08-2024",
+        notic_content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, voluptatem. Perferendis dolore placeat earum at!"
+    },
+
+]
