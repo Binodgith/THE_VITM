a6322820ff805c96091602694234c8316cf462a5
unknown <dbinod2020@gmail.com>
2024-10-19 20:05:55 +0530

    fetchrd

--- assets/scripts/career.js
+++ assets/scripts/career.js
@@ -1,5 +1,13 @@
-let career_opprtunity_list=[
+let career_opportunity_list=[
     {
-        
+        job_roll:"Assistant professor (Teaching staff)",
+        job_location:"At Institute (Berhampur)",
+        job_adv_no:"VITM-TS-00011",
+        job_status:"active",
+        job_released_date:"28-09-2024",
+        job_application_start:"01-10-2024",
+        job_application_ends:"30-11-2024",
+        job_appliaction_id:"2410"
+
     }
 ]
