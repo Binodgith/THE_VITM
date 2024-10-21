ce21b96021099952fe7048976b872c64c88b37a7
D <dbinod2020@gmail.com>
2024-10-22 02:28:07 +0530

    in announcement page apli fetching get complted by litul

--- assets/scripts/announcement.js
+++ assets/scripts/announcement.js
@@ -204,3 +204,17 @@ let announcement_list=[
     },
  
 ]
+
+
+function append_annoncement_list(data){
+    document.querySelector("#announcement-content-main>section").innerHTML=null;
+    data.map((ele)=>{
+        let announcement_box_main=document.createElement("div");
+        announcement_box_main.setAttribute("class","announcement-content-box-main");
+        announcement_box_main.innerHTML=`<a href=""><i class="fa-regular fa-clipboard"></i> &nbsp; <li>${ele.notice_title}</li></a>
+                            <p><span>Released on: </span>${ele.notice_release_date}</p>`;
+
+        document.querySelector("#announcement-content-main>section").append(announcement_box_main);
+    })
+}
+append_annoncement_list(announcement_list);
