eac54a35414b1bc983f3433a2f99a0757bdfd0cf
D <dbinod2020@gmail.com>
2024-10-21 22:31:36 +0530

    announcemet api creating started

---
+++ assets/scripts/announcement.js
@@ -0,0 +1,18 @@
+let announcement_list=[
+    {
+        notice_title:"Admission Date extended",
+        notice_description:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur aspernatur repudiandae doloribus fugit omnis tempora possimus voluptatum animi illum suscipit!",
+        notice_release_date:"15-10-2024",
+        notice_release_by:"Principal (dr.Bhimsen)",
+        notice_file_id:"dsdg784-3jhsdfg",
+        notice_id:"ancmt-0005"
+    },
+    {
+        notice_title:"College Holiday calender date changed",
+        notice_description:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur aspernatur repudiandae doloribus fugit omnis tempora possimus voluptatum animi illum suscipit!",
+        notice_release_date:"15-10-2024",
+        notice_release_by:"Principal (dr.Bhimsen)",
+        notice_file_id:"dsdg784-3jhsdfg",
+        notice_id:"ancmt-0008"
+    },
+]
