ab2b4df7db0a1f2e169e81be3d69d528a67bb4e4
litul020209 <litulubiswal@gmail.com>
2024-11-12 00:40:33 +0530

    complete link adding in carrer json file

--- components/Local JSON/career_opportunity_list.js
+++ components/Local JSON/career_opportunity_list.js
@@ -8,8 +8,8 @@ let career_opportunity_list=[
         job_application_start:"01-10-2024",
         job_application_ends:"30-11-2024",
         job_appliaction_id:"241025468",
-        job_short_description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae modi porro accusantium officiis veritatis, illo esse facere perferendis quaerat asperiores perspiciatis!",
-        job_prospectus_file_id:""
+        job_short_description:"Join our esteemed engineering college as an Assistant Professor! We’re looking for passionate educators in CSE, Mechanical, and EEE to inspire and guide the next generation of engineers. Apply today!",
+        job_prospectus_file_id:"1YaGlT0Al06wTcJPv6NhwSs5fEXRbSRXEYrfYbMCZd-Q"
     },
     
     {
@@ -21,8 +21,8 @@ let career_opportunity_list=[
         job_application_start:"01-10-2024",
         job_application_ends:"30-11-2024",
         job_appliaction_id:"241025469",
-        job_short_description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae modi porro accusantium officiis veritatis, illo esse facere perferendis quaerat asperiores perspiciatis!",
-        job_prospectus_file_id:""
+        job_short_description:"Join our team! We're seeking an experienced HOD for the ETC department to lead and inspire with strategic vision, innovation, and a strong commitment to academic excellence.",
+        job_prospectus_file_id:"16sjkn-VztuZxo68HNx00zKR7CkKyfEgDmAmSNSTahUc"
 
     },
     {
@@ -34,8 +34,8 @@ let career_opportunity_list=[
         job_application_start:"01-10-2024",
         job_application_ends:"30-11-2024",
         job_appliaction_id:"241056895",
-        job_short_description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae modi porro accusantium officiis veritatis, illo esse facere perferendis quaerat asperiores perspiciatis!",
-        job_prospectus_file_id:""
+        job_short_description:"Join our team as a skilled plumber at THE VITM! We seek a reliable professional to maintain campus facilities with quality service and expertise, ensuring a safe and functional environment for everyone.",
+        job_prospectus_file_id:"1vzAjUGzVNQOmycBRRnik4A8J7emRsTTsWbZBI7o-XUM"
 
     },
     {
@@ -47,8 +47,8 @@ let career_opportunity_list=[
         job_application_start:"01-10-2024",
         job_application_ends:"30-11-2024",
         job_appliaction_id:"241021154",
-        job_short_description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae modi porro accusantium officiis veritatis, illo esse facere perferendis quaerat asperiores perspiciatis!",
-        job_prospectus_file_id:""
+        job_short_description:"Join our team to manage and maintain data records efficiently. Strong attention to detail and proficiency in MS Excel required. Apply now to be part of our dynamic institution!",
+        job_prospectus_file_id:"1GQi7S60Tn3kTFTdUFl60lJZvsfpNMfljRSwXKgiWKAM"
 
     },
     {
@@ -60,8 +60,8 @@ let career_opportunity_list=[
         job_application_start:"01-10-2024",
         job_application_ends:"30-11-2024",
         job_appliaction_id:"241014587",
-        job_short_description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae modi porro accusantium officiis veritatis, illo esse facere perferendis quaerat asperiores perspiciatis!",
-        job_prospectus_file_id:""
+        job_short_description:"We are looking for a skilled electrician to join our team. The ideal candidate will have experience in electrical systems, maintenance, and troubleshooting. Apply now to be part of our dynamic campus team",
+        job_prospectus_file_id:"1lUVoh5AiEbdUmLXTy3TS8TpAWBtCmSqz742S8T-wy9I"
 
     },
     {
@@ -73,8 +73,8 @@ let career_opportunity_list=[
         job_application_start:"01-08-2024",
         job_application_ends:"30-09-2024",
         job_appliaction_id:"241056895",
-        job_short_description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae modi porro accusantium officiis veritatis, illo esse facere perferendis quaerat asperiores perspiciatis!",
-        job_prospectus_file_id:""
+        job_short_description:"Join our dynamic faculty at THE VITM! We are looking for passionate and skilled Assistant Professors in Computer Science, Engineering, and related fields to inspire the next generation of innovators",
+        job_prospectus_file_id:"1YaGlT0Al06wTcJPv6NhwSs5fEXRbSRXEYrfYbMCZd-Q"
 
     },
     {
@@ -86,8 +86,47 @@ let career_opportunity_list=[
         job_application_start:"01-10-2024",
         job_application_ends:"30-11-2024",
         job_appliaction_id:"241056895",
-        job_short_description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae modi porro accusantium officiis veritatis, illo esse facere perferendis quaerat asperiores perspiciatis!",
-        job_prospectus_file_id:""
+        job_short_description:"We are hiring passionate and dedicated professors for CSE, Mechanical, and EEE departments. Join us at THE VITM to contribute to academic excellence and shape the future of engineering education",
+        job_prospectus_file_id:"1_-17lhgysvNdbnrlGKkzyBPCyuEJRlDxyY0gJpjkDh4"
+
+    }
+    {
+        job_roll:"Security Guard (Non-Teaching staff)",
+        job_location:"At Institute (Berhampur)",
+        job_adv_no:"VITM-TS-00020",
+        job_status:"inactive",
+        job_released_date:"28-09-2024",
+        job_application_start:"01-10-2024",
+        job_application_ends:"30-11-2024",
+        job_appliaction_id:"241056895",
+        job_short_description:"We are hiring a vigilant and responsible Security Guard to ensure campus safety at THE VITM. This role involves monitoring campus premises, controlling access points, and maintaining a secure environment for students and staff.",
+        job_prospectus_file_id:"1lUVoh5AiEbdUmLXTy3TS8TpAWBtCmSqz742S8T-wy9I"
+
+    }
+    {
+        job_roll:"Canteen Incharge (Non-Teaching staff)",
+        job_location:"At Institute (Berhampur)",
+        job_adv_no:"VITM-TS-00020",
+        job_status:"inactive",
+        job_released_date:"28-09-2024",
+        job_application_start:"01-10-2024",
+        job_application_ends:"30-11-2024",
+        job_appliaction_id:"241056895",
+        job_short_description:"We're hiring a dedicated Canteen Incharge to oversee daily operations, ensure food quality, manage staff, and maintain hygiene standards in our college canteen. Join our team for a rewarding role!",
+        job_prospectus_file_id:"1lUVoh5AiEbdUmLXTy3TS8TpAWBtCmSqz742S8T-wy9I"
+
+    }
+    {
+        job_roll:"Accountant (Non-Teaching staff)",
+        job_location:"At Institute (Berhampur)",
+        job_adv_no:"VITM-TS-00020",
+        job_status:"inactive",
+        job_released_date:"28-09-2024",
+        job_application_start:"01-10-2024",
+        job_application_ends:"30-11-2024",
+        job_appliaction_id:"241056895",
+        job_short_description:"We're hiring a skilled Accountant to join our engineering college. Ideal candidates should excel in managing finances, maintaining records, and ensuring compliance. Join us to build a brighter future together!",
+        job_prospectus_file_id:"1GQi7S60Tn3kTFTdUFl60lJZvsfpNMfljRSwXKgiWKAM"
 
     }
 
