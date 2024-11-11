e221052cbdebd0c8cc83944eebd2b253e54eafe4
litul020209 <litulubiswal@gmail.com>
2024-11-11 22:17:04 +0530

    adding link in syllabus ,event ,annoucement pages

--- components/Local JSON/announcement_list.js
+++ components/Local JSON/announcement_list.js
@@ -4,8 +4,8 @@ let announcement_list=[
         notice_description:"We are pleased to announce that the deadline for BTech 2024 admissions has been extended to July 25, 2024, due to high demand. Prospective students who have not yet applied can submit their applications online through the admissions portal. Don't miss the opportunity to secure your seat!",
         notice_release_date:"29-06-2024",
         notice_release_by:"Principal (dr.Bhimsen)",
-        notice_file_id:"dsdg7841RanqysKCpdDfx_H2wtIIQDkL3PerOUKSAtKpayTNxC81RanqysKCpdDfx_H2wtIIQDkL3PerOUKSAtKpayTNxC8",
-        notice_file_embed_id:"2PACX-1vSpdoC5ajVwI_5qOBjwAQ6LkGhvOWTEimbgK-ZRsUPue-r_sfuNisFsWMfQnyHzh0ulB0L9lcjPnvYF",
+        notice_file_id:"1zGv0tGSARsrTmGeiueCrpved4M-XQdk_N670_evvB_g",
+        notice_file_embed_id:"2PACX-1vSLE4AE_IU2HywrnDE2pHOZyscis8ldHNavnh_ewYQeueAv3lMkjwfoF7PCtZZhUeiij_SQjCIWQfY9",
         notice_id:"ancmt-0005",
     },
     {
@@ -13,8 +13,8 @@ let announcement_list=[
         notice_description:"The college will observe its winter break from December 20, 2023, to January 5, 2024. No classes or administrative work will take place during this period. The new semester will begin on January 10, 2024, after a brief orientation session for students.",
         notice_release_date:"10-12-2024",
         notice_release_by:"Principal (dr.Bhimsen)",
-        notice_file_id:"dsdg7841RanqysKCpdDfx_H2wtIIQDkL3PerOUKSAtKpayTNxC81RanqysKCpdDfx_H2wtIIQDkL3PerOUKSAtKpayTNxC8",
-        notice_file_embed_id:"2PACX-1vSpdoC5ajVwI_5qOBjwAQ6LkGhvOWTEimbgK-ZRsUPue-r_sfuNisFsWMfQnyHzh0ulB0L9lcjPnvYF",
+        notice_file_id:"1KGBTPKnlsHixPuke5677FiQcSJSJa7aqnLS4vzGdp-A",
+        notice_file_embed_id:"2PACX-1vSGz086NOUFIoaMLDhCtxB83vzM0J2as3hsK-RGK_CZcMmzo4tacxfEfKtSPUlrC2AM0BROBU3SuaYy",
         notice_id:"ancmt-0008",
     },
 
@@ -23,8 +23,8 @@ let announcement_list=[
         notice_description:"Our Annual Tech Fest, AKASH 2K24 , will take place from December 12 to 14, 2024. Registrations for events and competitions will open on November 1. Students from all departments are encouraged to participate and showcase their talent.",
         notice_release_date:"22-11-2024",
         notice_release_by:"Principal (dr.Bhimsen)",
-        notice_file_id:"1RanqysKCpdDfx_H2wtIIQDkL3PerOUKSAtKpayTNxC8",
-        notice_file_embed_id:"2PACX-1vSpdoC5ajVwI_5qOBjwAQ6LkGhvOWTEimbgK-ZRsUPue-r_sfuNisFsWMfQnyHzh0ulB0L9lcjPnvYF",
+        notice_file_id:"1wSrSOCpKFHVr3tzTmuxx1-FEqfUJKf0FdOu8h7_mI9w",
+        notice_file_embed_id:"2PACX-1vTQIi-1QvmmbUCYUnb99EyTJKB434qSileosNnsyGYGHKe3hSrhK47fwQCUfMY4qvvxjpq5sWKUCNwb",
         notice_id:"ancmt-0011",
     },
     {
@@ -32,8 +32,8 @@ let announcement_list=[
         notice_description:"The annual Sports Week will be held from November 20 to 25, 2024. Events include football, cricket, badminton, and table tennis. Students can register their teams on the sports page by  before November 09 . Let's promote fitness and teamwork! ",
         notice_release_date:"18-11-2024",
         notice_release_by:"Principal (dr.Bhimsen)",
-        notice_file_id:"1RanqysKCpdDfx_H2wtIIQDkL3PerOUKSAtKpayTNxC8",
-        notice_file_embed_id:"2PACX-1vSpdoC5ajVwI_5qOBjwAQ6LkGhvOWTEimbgK-ZRsUPue-r_sfuNisFsWMfQnyHzh0ulB0L9lcjPnvYF",
+        notice_file_id:"1yzF-zbsoo4zVIySrLr38qemHk0TOccYAqyT-taI3PHQ",
+        notice_file_embed_id:"2PACX-1vQ8idEdkvlw7VEVgYerSKP49N9O2q3OCTOatyu-bfprEjp5r8i7ilb16i3tNU_Fv1NndNu1MzEEYOQf",
         notice_id:"ancmt-0014",
     },
     {
@@ -41,8 +41,8 @@ let announcement_list=[
         notice_description:"The Training and Placement Cell has announced internship opportunities with leading tech companies. Interested students should submit their resumes to the T&P office by October 25. Interviews will be conducted in November.",
         notice_release_date:"23-09-2024",
         notice_release_by:"Principal (dr.Bhimsen)",
-        notice_file_id:"1RanqysKCpdDfx_H2wtIIQDkL3PerOUKSAtKpayTNxC8",
-        notice_file_embed_id:"",
+        notice_file_id:"1tjOZ6COHl0OY4RqzQeOpqYRFU2lm7fyocrFSX6irYrY",
+        notice_file_embed_id:"2PACX-1vQjb2iBV31hLA-WZIoZBco6dxpB3VnwUKZA0_XICK2x_Wr-VRWjDLbhY7rxuDuOnlg4zKvntuJDmv61",
         notice_id:"ancmt-0017"
     },
     {
@@ -50,8 +50,8 @@ let announcement_list=[
         notice_description:"The college is organizing a 24-hour coding hackathon from November 15 to 16, 2024. The hackathon is open to students from all years. Teams will work on real-world problem statements and compete for cash prizes. Registration closes on November 10.",
         notice_release_date:"20-10-2024",
         notice_release_by:"Principal (dr.Bhimsen)",
-        notice_file_id:"1RanqysKCpdDfx_H2wtIIQDkL3PerOUKSAtKpayTNxC8",
-        notice_file_embed_id:"",
+        notice_file_id:"1tjOZ6COHl0OY4RqzQeOpqYRFU2lm7fyocrFSX6irYrY",
+        notice_file_embed_id:"2PACX-1vQjb2iBV31hLA-WZIoZBco6dxpB3VnwUKZA0_XICK2x_Wr-VRWjDLbhY7rxuDuOnlg4zKvntuJDmv61",
         notice_id:"ancmt-0008"
     },
     {
@@ -59,8 +59,8 @@ let announcement_list=[
         notice_description:"The examination schedule for the Fall 2024 semester has been released. Students can access the detailed timetable in the examination section of the website. Exams will start from November 15, 2024. Make sure to review your subjects and prepare well in advance. Any conflicts should be reported by October 25, 2024.",
         notice_release_date:"28-10-2024",
         notice_release_by:"Principal (dr.Bhimsen)",
-        notice_file_id:"1RanqysKCpdDfx_H2wtIIQDkL3PerOUKSAtKpayTNxC81RanqysKCpdDfx_H2wtIIQDkL3PerOUKSAtKpayTNxC8",
-        notice_file_embed_id:"",
+        notice_file_id:"1zGv0tGSARsrTmGeiueCrpved4M-XQdk_N670_evvB_g",
+        notice_file_embed_id:"2PACX-1vSLE4AE_IU2HywrnDE2pHOZyscis8ldHNavnh_ewYQeueAv3lMkjwfoF7PCtZZhUeiij_SQjCIWQfY9",
         notice_id:"ancmt-0020"
     },
     {
@@ -68,8 +68,8 @@ let announcement_list=[
         notice_description:"The Annual Alumni Meet will be held on December 20, 2024. Alumni from all batches are invited to reconnect with their peers and faculty. The event will feature a panel discussion, networking sessions, and a cultural evening. Register online by December 10.",
         notice_release_date:"26-11-2024",
         notice_release_by:"Principal (dr.Bhimsen)",
-        notice_file_id:"1RanqysKCpdDfx_H2wtIIQDkL3PerOUKSAtKpayTNxC81RanqysKCpdDfx_H2wtIIQDkL3PerOUKSAtKpayTNxC8",
-        notice_file_embed_id:"",
+        notice_file_id:"16TdTinkwQ9Wq-DZrJVXIIi4Foz8Ml7W7IY6jGkWZChg",
+        notice_file_embed_id:"2PACX-1vRXGhS1KgNrnqb9vDpD4Hd9FEdAFVsHRPfswxUtQ_mnN0llDvrgkeyQY9qMnzMQj8w4SfN_pvRdiBkP",
         notice_id:"ancmt-0023"
     },
     {
@@ -77,8 +77,8 @@ let announcement_list=[
         notice_description:"An inter-college online quiz competition will be held on November 10, 2024. Topics will include general knowledge, current affairs, and technical subjects. Winners will receive cash prizes and certificates. Register on the events page by November 5.",
         notice_release_date:"31-10-2024",
         notice_release_by:"Principal (dr.Bhimsen)",
-        notice_file_id:"1RanqysKCpdDfx_H2wtIIQDkL3PerOUKSAtKpayTNxC81RanqysKCpdDfx_H2wtIIQDkL3PerOUKSAtKpayTNxC8",
-        notice_file_embed_id:"",
+        notice_file_id:"19TpUssHSJBHMpYUeNG2x9gVAY-W6s3gUihLTSFe44BY",
+        notice_file_embed_id:"2PACX-1vQ3uD6lALsRrheyCwN3t0gIiSFy3jTqibX63OkG7KKteyOS0nXDUZxwDz-KSHCndgmSWE4xCBzsFSZg",
         notice_id:"ancmt-0026"
     },
     {
@@ -95,8 +95,8 @@ let announcement_list=[
         notice_description:"Applications for merit-based and need-based scholarships for the academic year 2024-2025 are now open. Interested students should apply through the scholarship portal by October 31. Submit all required documents for verification.",
         notice_release_date:"03-10-2024",
         notice_release_by:"Principal (dr.Bhimsen)",
-        notice_file_id:"1RanqysKCpdDfx_H2wtIIQDkL3PerOUKSAtKpayTNxC81RanqysKCpdDfx_H2wtIIQDkL3PerOUKSAtKpayTNxC8",
-        notice_file_embed_id:"",
+        notice_file_id:"1ECXWRw3iovMyqkT5DPjTrQI4EMonfIwQFMzJplys-Pc",
+        notice_file_embed_id:"2PACX-1vRC-G4tGvRwd1bbLB1bhMPkUbM1eLoKqOq-wT0B14FHSNcgMRKjeEmvryOzW9qTuzAHz0cxfTLjdU1k",
         notice_id:"ancmt-0032"
     },
     {
@@ -104,8 +104,8 @@ let announcement_list=[
         notice_description:"A campus recruitment drive will be held from November 20 to 25, 2024, for final-year students. Companies from various sectors, including IT, manufacturing, and finance, will participate. Prepare your resumes and register with the placement cell.",
         notice_release_date:"15-10-2024",
         notice_release_by:"Principal (dr.Bhimsen)",
-        notice_file_id:"1RanqysKCpdDfx_H2wtIIQDkL3PerOUKSAtKpayTNxC81RanqysKCpdDfx_H2wtIIQDkL3PerOUKSAtKpayTNxC8",
-        notice_file_embed_id:"",
+        notice_file_id:"145GJTjBa8QfOYBWihfa9Mydr2boDEnjO4elPwi4lK4w",
+        notice_file_embed_id:"2PACX-1vSP2Y1UG1EMlpDQg31AQIhlgnf5fjtdT6ypnW4BWAb07_FTBGapspo_PBF2Vb6GSnz847dXyRPyfcKe",
         notice_id:"ancmt-0035"
     },
     {
@@ -132,8 +132,8 @@ let announcement_list=[
         notice_description:"The Freshers' Party for the 2024-2025 batch will be held on October 29, 2024. The event will include performances, fun games, and a DJ night. All first-year students are invited. Contact your class representative for more details.",
         notice_release_date:"30-09-2024",
         notice_release_by:"Principal (dr.Bhimsen)",
-        notice_file_id:"1RanqysKCpdDfx_H2wtIIQDkL3PerOUKSAtKpayTNxC81RanqysKCpdDfx_H2wtIIQDkL3PerOUKSAtKpayTNxC8",
-        notice_file_embed_id:"",
+        notice_file_id:"1Y9xuNBmOxOoKIGtxyGC7LFO0uIyK2lTeduQUNOOAIeE",
+        notice_file_embed_id:"2PACX-1vQidFYsEpf8QCyBCOgb1yWlZu2ZusDzDR1qxj03DsBcQ9xm8t44TnEMRiKQjd-WA_Wx9yJURdHOldWs",
         notice_id:"ancmt-0044"
     },
 
@@ -143,7 +143,8 @@ let announcement_list=[
         notice_release_date:"15-10-2024",
         notice_release_by:"Principal (dr.Bhimsen)",
         notice_file_id:"dsdg784-3jhsdfg",
-        notice_id:"ancmt-0047"
+        notice_file_embed_id:"",
+        notice_id:"ancmt-0047",
     },
     {
         notice_title:"Workshop on AI & ML",
@@ -158,7 +159,8 @@ let announcement_list=[
         notice_description:"The college will remain closed from October 31 to November 02 , 2024, for Diwali holidays. Classes will resume on November 03 . Students are advised to submit all pending assignments before the break.",
         notice_release_date:"15-10-2024",
         notice_release_by:"Principal (dr.Bhimsen)",
-        notice_file_id:"dsdg784-3jhsdfg",
+        notice_file_id:"1tpO6iX1UKXrKcZ-pBQVu3xgTkQHrbPrvIqP_guO0Syg",
+        notice_file_embed_id:"2PACX-1vS6ajqcq0yWu4v-l82ZQCvdMdYkwz_ZxSyR2lfm4Vp0kCmXbhAYAa4AnHl1-q9hB-TJmjbO-hgi8idE",
         notice_id:"ancmt-0053"
     },
     {
@@ -174,7 +176,8 @@ let announcement_list=[
         notice_description:"Applications for the Summer Internship Program are now open. Students can apply to leading companies for internships in fields such as software development, electronics, and mechanical engineering. Submit your applications by April 15, 2024",
         notice_release_date:"15-10-2024",
         notice_release_by:"Principal (dr.Bhimsen)",
-        notice_file_id:"dsdg784-3jhsdfg",
+        notice_file_id:"1tjOZ6COHl0OY4RqzQeOpqYRFU2lm7fyocrFSX6irYrY",
+        notice_file_embed_id:"2PACX-1vQjb2iBV31hLA-WZIoZBco6dxpB3VnwUKZA0_XICK2x_Wr-VRWjDLbhY7rxuDuOnlg4zKvntuJDmv61",
         notice_id:"ancmt-0059"
     },
     {
@@ -182,7 +185,8 @@ let announcement_list=[
         notice_description:"The final-year project submission deadline has been extended to November  10 , 2024. Make sure your projects are complete and submitted before the deadlin",
         notice_release_date:"19-10-2024",
         notice_release_by:"Principal (dr.Bhimsen)",
-        notice_file_id:"dsdg784-3jhsdfg",
+        notice_file_id:"1Q8ic6LxJ8Rc7mRagbveKSfMYVDFt-4QSjOfWw88Dj2M",
+        notice_file_embed_id:"2PACX-1vQOyCxFCCxWRLUyFdIf_MD3rq7mbaDd2HVPB0Pi2bb4MoKqaLuef7hQRKQ35rLkpsdsSblIetUtgyxR",
         notice_id:"ancmt-0062"
     },
     {
@@ -190,7 +194,8 @@ let announcement_list=[
         notice_description:"The college has updated its hostel rules. Students are required to adhere to the new curfew timings and regulations. Violations may result in penalties.",
         notice_release_date:"30-08-2024",
         notice_release_by:"Principal (dr.Bhimsen)",
-        notice_file_id:"dsdg784-3jhsdfg",
+        notice_file_id:"145GJTjBa8QfOYBWihfa9Mydr2boDEnjO4elPwi4lK4w",
+        notice_file_embed_id:"2PACX-1vSP2Y1UG1EMlpDQg31AQIhlgnf5fjtdT6ypnW4BWAb07_FTBGapspo_PBF2Vb6GSnz847dXyRPyfcKe",
         notice_id:"ancmt-0065"
     },
     {
