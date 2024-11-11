8e9bf1cb7cc201217512612881fa71e00f23c67f
Codegirl1626 <smrutim481@gmail.com>
2024-11-11 20:57:08 +0530

    put real data

--- components/Local JSON/staff_faculty_list.js
+++ components/Local JSON/staff_faculty_list.js
@@ -1,86 +1,86 @@
 let staff_faculty_list=[
     
-              {name: "John Doe",
-              designation: "Professor of Mathematics",
-              phone: "+1234567890",
-              email: "johndoe@example.com",
+              {name: "Prof.Dr.K.Govinda Rajalu",
+              designation: "Chairman",
+              phone: "9437011046",
+              email: "chairman@vitam.edu.in",
               image: "../assets/media/faculty.img/faculty.1.jpg"
             },
             {
-              name: "Jane Smith",
-              designation: "Assistant Professor of Physics",
-              phone: "+1234567891",
-              email: "janesmith@example.com",
+              name: "Dr.MBNV Prasad",
+              designation: "Secretary",
+              phone: "9861875481",
+              email: "secretary@vitam.edu.in",
               image: "../assets/media/faculty.img/faculty.2.jpeg"
             },
             {
-              name: "Michael Johnson",
-              designation: "HOD of Computer Science",
-              phone: "+1234567892",
-              email: "michaeljohnson@example.com",
+              name: "Prof.Dr.G.Eswar Rao",
+              designation: "Principal",
+              phone: "9861875935",
+              email: "principal@vitam.edu.in",
               image: "../assets/media/faculty.img/faculty.3.jpeg"
             },
             {
-              name: "Emily Davis",
-              designation: "Professor of English",
-              phone: "+1234567893",
-              email: "emilydavis@example.com",
+              name: "Prof.B.Srinivas Rao",
+              designation: "HOD CSE & EEE",
+              phone: "8961817814",
+              email:"rao.bsrinivas@gmail.com",
               image: "../assets/media/faculty.img/faculty.4.jpeg"
             },
             {
-              name: "Robert Wilson",
-              designation: "Head of MBA Department",
-              phone: "+1234567894",
-              email: "robertwilson@example.com",
+              name: "Prof.Sunil Kumar Dash",
+              designation: "HOD ETC",
+              phone: "9437338358",
+              email: "dassunil2003@gmail.com",
               image: "../assets/media/faculty.img/faculty.5.jpeg"
             },
             {
-              name: "Sophia Martinez",
-              designation: "Assistant Professor of Chemistry",
-              phone: "+1234567895",
-              email: "sophiamartinez@example.com",
+              name: "Prof.Dr.Anup Kumar Panda",
+              designation: "HOD MBA",
+              phone: "9937026533",
+              email: "mbahod.vitam@gmail.com",
               image: "../assets/media/faculty.img/faculty.6.jpeg"
             },
             {
-              name: "William Brown",
-              designation: "HOD of Civil Department",
-              phone: "+1234567896",
-              email: "williambrown@example.com",
+              name: "Prof.Dr.Prabhu Prasad Mishra",
+              designation: "HOD Mechanical",
+              phone: "7008434164",
+              email: "prabhumishra28@gmail.com",
               image: "../assets/media/faculty.img/faculty.7.jpg"
             },
             {
-              name: "Olivia Garcia",
-              designation: "HOD of Mechanical Department",
-              phone: "+1234567897",
-              email: "oliviagarcia@example.com",
+              name: "Prof.Monika Das",
+              designation: "HOD Civil",
+              phone: "8118099241",
+              email: "monikadas13@gmail.com",
               image: "../assets/media/faculty.img/faculty.8.jpeg"
             },
             {
-              name: "Edward Tample",
-              designation: "Principal",
-              phone: "+1234567897",
-              email: "edwardtample@example.com",
+              name: "Ass.Prof.Anuradha Pani",
+              designation: "Ass.Prof CSE",
+              phone: "7606838391",
+              email: "anuradha.pani24@gmail.com",
               image: "../assets/media/faculty.img/principal.jpg"
             },
             {
-              name: "Dr. Chals William",
-              designation: "Chairman",
-              phone: "+1234567897",
-              email: "chalswilliam@example.com",
+              name: "Ass.Prof.Sunanda Nayak",
+              designation: "Ass.Prof.CSE",
+              phone: "9178882207",
+              email: "sunandanayak927@gmail.com",
               image: "../assets/media/faculty.img/college-chairman-img1.webp"
             },
             {
-              name: "Dr. Sarah Thompson",
-              designation: "HOD of ECE",
-              phone: "+1234567898",
-              email: "sarahthompson@example.com",
+              name: "Ass.Prof.Pradhan Bage",
+              designation: "Ass.Prof CSE",
+              phone: "9556262783",
+              email: "pradhan.bage@gmail.com",
               image: "../assets/media/faculty.img/faculty.11.jpg"
             },
             {
-              name: "Dr. Alex Chen",
-              designation: "HOD of EEE",
-              phone: "+1234567899",
-              email: "alexchen@example.com",
+              name: "Ass.Prof.Srikanta Sabat",
+              designation: "Ass.Prof. CSE",
+              phone: "9776705368",
+              email: "srikantakumar1206@gmail.com",
               image: "../assets/media/faculty.img/faculty.12.jpg"
             },
             {
