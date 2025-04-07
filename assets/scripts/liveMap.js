541ceb84b5bb4a6a8877cb20d82c54fd28263a43
unknown <dbinod2020@gmail.com>
2025-04-07 14:26:59 +0530

    rote path added

--- assets/scripts/liveMap.js
+++ assets/scripts/liveMap.js
@@ -4,23 +4,63 @@
    
    let map = L.map('map',{center:[19.3150, 84.7941],zoom:20});
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    // L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    //     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    // }).addTo(map);
+    
+    // L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.{ext}', {
+    //     attribution: '&copy; CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver (Contains Copernicus Data) | &copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    //     ext: 'jpg'
+    // }).addTo(map);
+
+    // L.tileLayer('https://tileserver.memomaps.de/tilegen/{z}/{x}/{y}.png', {
+    //     // maxZoom: 18,
+    //     attribution: 'Map <a href="https://memomaps.de/">memomaps.de</a> <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    // }).addTo(map);
+
+
+    // L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    //     attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+    // }).addTo(map);
+
+    // L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+        
+    //     attribution: '&copy; OpenStreetMap France | &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    // }).addTo(map);
+
+    L.tileLayer('https://tile.openstreetmap.de/{z}/{x}/{y}.png', {
+        maxZoom: 18,
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
     var myIcon = L.icon({
-        iconUrl: 'https://i.ibb.co/Csn1WqJs/ac0ho6cs-copy.png',
-        iconSize: [25, 45],
-        iconAnchor: [15,25],
+        iconUrl: 'https://cdn2.iconfinder.com/data/icons/map-locations-colored-outlined-pixel-perfect/64/pin-map-location-06-1024.png',
+        iconSize: [50, 75],
+        iconAnchor: [30,60],
         // popupAnchor: [-3, -76],
         // shadowUrl: 'https://cdn-icons-png.flaticon.com/512/9249/9249336.png',
         // shadowSize: [68, 95],
         shadowAnchor: [25, 25]
     });
-    let marker=L.marker([19.3150, 84.7941],{zoom:13}).addTo(map);
-
     
+    let marker=L.marker([19.314922, 84.794218],{icon:myIcon,zoom:13}).addTo(map).bindPopup("Your live position");
+    L.Routing.control({
+        waypoints: [
+          L.latLng(19.311041, 84.831311),
+          L.latLng(19.310054, 84.828033),
+          L.latLng(19.308730, 84.823079),
+          L.latLng(19.307837, 84.818726),
+          L.latLng(19.307296, 84.815483),
+          L.latLng(19.306865, 84.812549),
+          L.latLng(19.306313, 84.809332),
+          L.latLng(19.306639, 84.808439),
 
+          L.latLng(19.308523, 84.806591),
+          L.latLng(19.310691, 84.804612),
+          L.latLng(19.313225, 84.802311),
+        
+        ]
+      },).addTo(map);
 
     // --------------ma layers and mods---------------------
    
@@ -40,8 +80,6 @@ function getposition(pos) {
     long=pos.coords.longitude;
     lat=pos.coords.latitude;
     console.log(pos.coords);
-
-
     setmarker();
     // showonmap(lat,long);
     // document.getElementById("map").innerHTML = `<embed src="https://www.google.com/maps?q=${pos.coords.latitude},${pos.coords.longitude}&z=20&output=embed" type="" width="90%" height="550vh"> `
@@ -68,11 +106,11 @@ function getposition(pos) {
 
 function setmarker(){
     
-    map.setView([lat,long],20);
+    map.flyTo([lat,long],18);
     
     let newLatLng = new L.LatLng(lat, long);
     
-    L.circle([lat,long], {radius: 30}).addTo(map);  
+    // L.circle([lat,long], {radius: 30}).addTo(map);  
 
 
 
