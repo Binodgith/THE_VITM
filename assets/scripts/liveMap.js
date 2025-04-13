553773541cccd6a57864fbad20b2fd72fc80ab1a
unknown <dbinod2020@gmail.com>
2025-04-13 20:05:05 +0530

    live map pages designed

--- assets/scripts/liveMap.js
+++ assets/scripts/liveMap.js
@@ -1,10 +1,131 @@
+import { getRouteDetails } from "../../db/Transportation DB/getRouteDetails.js";
 
 
-   let map = L.map('map',{center:[19.3150, 84.7941],zoom:17});
+    let busN="B03";
+    let rw="4";
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+
+    async function getRoute(busN){
+        let res= await getRouteDetails(busN);
+        console.log(res)
+        console.log(res[1]["Latitude"]);
+        
+        embedRout(res);
+        AllignStopages(res);
+    }
+
+    getRoute(busN);
+
+
+    let map = L.map('map',{center:[19.245839,84.765546],zoom:5});
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        var busStopIcon = L.icon({
+            iconUrl: '/assets/icons/busStop.png',
+            iconSize: [50, 75],
+            iconAnchor: [30,60],
+            // popupAnchor: [-3, -76],
+            // shadowUrl: 'https://cdn-icons-png.flaticon.com/512/9249/9249336.png',
+            // shadowSize: [68, 95],
+            shadowAnchor: [25, 25]
+        });
+
+        var myBusIcon = L.icon({
+            iconUrl: 'https://cdn2.iconfinder.com/data/icons/map-locations-colored-outlined-pixel-perfect/64/pin-map-location-06-1024.png',
+            iconSize: [50, 75],
+            iconAnchor: [30,60],
+            // popupAnchor: [-3, -76],
+            // shadowUrl: 'https://cdn-icons-png.flaticon.com/512/9249/9249336.png',
+            // shadowSize: [68, 95],
+            shadowAnchor: [25, 25]
+        });
+
+
+
+
+
+    function embedRout(res){
+        
+
+        // let marker=L.marker([19.245839,84.765546],{zoom:20}).addTo(map).bindPopup("Your live position");
+        let points=[];
+        res.map(pos=>{
+            let x=L.latLng(pos["Latitude"], pos["Longitude"]);
+            points.push(x);
+        }) 
+
+        let myRoute=L.Routing.control({
+            waypoints: points,
+                
+            lineOptions: {
+                styles: [{color: 'Green', opacity: 1, weight: 10}]
+            },
+    
+            
+            createMarker: function (i, start, n){ 
+                //for (i = 0; waypoint.length; i++){
+                return L.marker (start.latLng, {
+                        draggable: false,
+                        icon: busStopIcon,
+                        iconSize: [25, 25],
+                       routeWhileDragging: false,
+                }).bindPopup(res[i]["Stopage_Name"]+","+res[i]["Note"]);
+            }   
+                        
+        }).addTo(map);
+
+
+
+
+
+
+    }  
+
+
+
+    function setmarker(){
+    
+        map.flyTo([lat,long],17);
+        
+        let newLatLng = new L.LatLng(lat, long);
+        
+        // L.circle([lat,long], {radius: 30}).addTo(map);  
+    
+    
+    
+        marker.setLatLng(newLatLng);
+    
+    
+    }
+
+
+
+
+
+
+    function AllignStopages(res){
+        document.querySelector(".rb-container .rb").innerHTML='';
+        res.map(pos=>{
+            let StoageLI=document.createElement("li");
+            StoageLI.setAttribute("class","rb-item");
+            StoageLI.setAttribute("ng-repeat","itembx");
+
+            StoageLI.innerHTML=`<div class="item-title">${pos.Stopage_Name}</div>
+                <div class="timestamp">
+                   00:00 PM
+                </div>`
+
+            document.querySelector(".rb-container .rb").append(StoageLI);
+
+            
+        })
+    }
+
+
+ 
     
     // L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.{ext}', {
     //     attribution: '&copy; CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver (Contains Copernicus Data) | &copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
@@ -32,92 +153,44 @@
     // }).addTo(map);
 
 
-    let marker=L.marker([19.314922, 84.794218],{zoom:20}).addTo(map).bindPopup("Your live position");
 
 
 
     // --------------------------------------------------bus live Icon-------------------------
-    var myIcon = L.icon({
-        iconUrl: 'https://cdn2.iconfinder.com/data/icons/map-locations-colored-outlined-pixel-perfect/64/pin-map-location-06-1024.png',
-        iconSize: [50, 75],
-        iconAnchor: [30,60],
-        // popupAnchor: [-3, -76],
-        // shadowUrl: 'https://cdn-icons-png.flaticon.com/512/9249/9249336.png',
-        // shadowSize: [68, 95],
-        shadowAnchor: [25, 25]
-    });
-
+  
 
 
     // --------------------------------------------------bus stopage point icon----------------------
-    var busStopIcon = L.icon({
-        iconUrl: '/assets/icons/busStop.png',
-        iconSize: [50, 75],
-        iconAnchor: [30,60],
-        // popupAnchor: [-3, -76],
-        // shadowUrl: 'https://cdn-icons-png.flaticon.com/512/9249/9249336.png',
-        // shadowSize: [68, 95],
-        shadowAnchor: [25, 25]
-    });
+   
     
 
 
 //    ----------------------------------------------------------Assigning routing service------------------------------
-    let myRoute=L.Routing.control({
-        waypoints: [
-          L.latLng(19.311041, 84.831311),
-          L.latLng(19.310054, 84.828033),
-          L.latLng(19.308730, 84.823079),
-          L.latLng(19.307837, 84.818726),
-          L.latLng(19.307296, 84.815483),
-          L.latLng(19.306865, 84.812549),
-          L.latLng(19.306313, 84.809332),
-          L.latLng(19.306639, 84.808439),
-
-          L.latLng(19.308523, 84.806591),
-          L.latLng(19.310691, 84.804612),
-          L.latLng(19.313225, 84.802311),
-        
-        ],
-        lineOptions: {
-            styles: [{color: 'Green', opacity: 1, weight: 10}]
-        },
-
-        
-        createMarker: function (i, start, n){ 
-            //for (i = 0; waypoint.length; i++){
-            return L.marker (start.latLng, {
-                    draggable: false,
-                    icon: busStopIcon,
-                    iconSize: [25, 25],
-                   routeWhileDragging: false,
-        })}
-                    
-    }).addTo(map);
+    
 
     
 
    
 
-function getlocation() {
-    // console.log(navigator.geolocation);
-    navigator.geolocation.getCurrentPosition(getposition);
-    // if (navigator.getlocation) {
+// function getlocation() {
+//     // console.log(navigator.geolocation);
+//     navigator.geolocation.getCurrentPosition(getposition);
+//     // if (navigator.getlocation) {
 
-    // }
-}
+//     // }
+// }
  
-getlocation();
+// getlocation();
 
-let  long,lat;
-function getposition(pos) {
-    long=pos.coords.longitude;
-    lat=pos.coords.latitude;
-    console.log(pos.coords);
-    setmarker();
-    // showonmap(lat,long);
-    // document.getElementById("map").innerHTML = `<embed src="https://www.google.com/maps?q=${pos.coords.latitude},${pos.coords.longitude}&z=20&output=embed" type="" width="90%" height="550vh"> `
-}
+// let  long,lat;
+// function getposition(pos) {
+//     long=pos.coords.longitude;
+//     lat=pos.coords.latitude;
+//     console.log(pos.coords);
+//     setmarker();
+//     // showonmap(lat,long);
+//     // document.getElementById("map").innerHTML = `<embed src="https://www.google.com/maps?q=${pos.coords.latitude},${pos.coords.longitude}&z=20&output=embed" type="" width="90%" height="550vh"> `
+// }
 
 
 // function getposition(pos){
@@ -138,20 +211,7 @@ function getposition(pos) {
 
    
 
-function setmarker(){
-    
-    map.flyTo([lat,long],17);
-    
-    let newLatLng = new L.LatLng(lat, long);
-    
-    // L.circle([lat,long], {radius: 30}).addTo(map);  
 
-
-
-    marker.setLatLng(newLatLng);
-
-    getlocation();
-}
 
 
 
